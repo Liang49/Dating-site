b671a7afbe85063349d0c17c2080ea1ef50dcd9b
Liang49 <liangzhang2014@gmail.com>
2021-03-10 22:01:20 -0500

    login page

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,8 +6,23 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className="home-page">
-        <h1 style={{ color: "red", textAlign: "center" }}> Find You Love</h1>
-        <h1 style={{ color: "red", textAlign: "center" }}>
+        <h1
+          style={{
+            color: "red",
+            textAlign: "center",
+            fontFamily: "Open Sans sans-serif"
+          }}
+        >
+          {" "}
+          Find You Love
+        </h1>
+        <h1
+          style={{
+            color: "red",
+            textAlign: "center",
+            fontFamily: "Open Sans sans-serif"
+          }}
+        >
           Your fated one may be here
         </h1>
 
--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 
 class Login extends React.Component {
@@ -8,18 +9,15 @@ class Login extends React.Component {
         <Form>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <br />
             <Form.Control type="email" placeholder="Enter email" />
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <br />
-
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
+          <Button>Login</Button>
         </Form>
-        <div className="Login"></div>
       </div>
     );
   }
